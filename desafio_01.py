ce4a904c62d8c2f9678dfb26efe3743b99b416cd
thomthomtt <thomazgt39@gmail.com>
2025-04-03 16:54:57 -0300

    desafio01

---
+++ desafio_01.py
@@ -0,0 +1,17 @@
+entrada_h1 = int(input("Digite a primeira hora: "))
+entrada_min1 = int(input("Digite os minutos da primeira hora:"))
+entrada_h2 = int(input("Digite a segunda hora:"))
+entrada_min2 = int(input("Digite os minutos da segunda hora:"))
+soma_minutos = entrada_min1+entrada_min2
+if entrada_h1 >= 12:
+    entrada_h1 = entrada_h1 - 12
+if entrada_h2 >= 12:
+    entrada_h2 = entrada_h2 - 12
+soma_horas = entrada_h1 + entrada_h2
+if soma_minutos >= 60:
+    soma_horas = soma_horas+1
+    soma_minutos = soma_minutos - 60
+print(f"{soma_horas}:{soma_minutos}")
+
+
+
